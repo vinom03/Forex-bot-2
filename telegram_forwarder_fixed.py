e96b10e8e8db628b301e2da7bf05ee7b64fce454
vinom03 <vinom.03.vin@gmail.com>
2026-08-02 02:05:08 +0300

    Update telegram_forwarder_fixed.py

--- telegram_forwarder_fixed.py
+++ telegram_forwarder_fixed.py
@@ -573,6 +573,7 @@ def run_once():
 
                 # لو المنشور رد على منشور ثاني، نشوف هل عندنا "نسخته"
                 # بقناتك بدفتر المطابقة - لو عندنا، نرد عليه بنفس الشكل
+            
             reply_to_dest_id = None
                 if post.get("reply_to"):
                     reply_to_dest_id = id_map.get(str(post["reply_to"]))
